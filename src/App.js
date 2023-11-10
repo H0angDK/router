0d719514f13f528976b88ae2c82e48072ae00ce5
H0angDK <dinkonghoang510@gmail.com>
2023-11-10 20:20:04 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Users, loader as usersLoader } from "./components/User/Users";
+import { Albums, loader as albumsLoader } from "./components/Album/Albums";
+import { UserPage, loader as userLoader } from "./components/User/UserPage";
+import { AlbumPage, loader as albumLoader } from "./components/Album/AlbumPage";
+import { Error } from "./components/Error";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        loader: async () => redirect("/users")
+      },
+      {
+        path: 'albums',
+        children: [
+          {
+            index: true,
+            element: <Albums />,
+            loader: albumsLoader
+          },
+          {
+            path: ':id',
+            element: <AlbumPage />,
+            id: "album",
+            loader: albumLoader
+          }
+        ],
+      },
+      {
+        path: 'users',
+        children: [
+          {
+            index: true,
+            element: <Users />,
+            loader: usersLoader,
+          },
+          {
+            path: ':id',
+            element: <UserPage />,
+            loader: userLoader,
+          }
+        ]
+      }
+    ]
+  }
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
